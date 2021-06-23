9e920fd37e9f0d63f481eee51f2b995e17acb963
thecargos <loayzavelez@gmail.com>
2021-06-23 02:03:32 -0500

    feat(App): defining the app structure folders

---
+++ src/layouts/AppLayout/AppLayout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import styles from './styles.module.scss'
+
+const AppLayout = () => {
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  )
+}
+
+export default AppLayout
